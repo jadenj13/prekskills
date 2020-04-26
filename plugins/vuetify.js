2b2fee6c3d679c8769069aa6fd35e3e83720349c
jadenj133 <jadenj133@gmail.com>
2020-04-25 23:29:00 -0600

    moves vuetify theme to plugin

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,30 +1,22 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import colors from 'vuetify/es5/util/colors';
 import 'vuetify/dist/vuetify.min.css';
 
-Vue.use(Vuetify);
-
 const opts = {
-  customVariables: ['~/assets/variables.scss'],
+  iconFonts: 'mdi',
   theme: {
-    themes: {
-      light: {
-        primary: colors.blue.darken2,
-        accent: colors.green.darken2,
-        secondary: colors.purple.darken2,
-        info: colors.teal.lighten1,
-        warning: colors.amber.base,
-        error: colors.deepOrange.accent4,
-        success: colors.green.accent3,
-      },
-    },
-    options: {
-      customProperties: true,
-    },
+    primary: colors.blue.darken2,
+    accent: colors.green.darken2,
+    secondary: colors.purple.darken2,
+    info: colors.teal.lighten1,
+    warning: colors.amber.base,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3,
   },
-  icons: {
-    iconFont: 'md',
+  options: {
+    customProperties: true,
   },
 };
 
-export default new Vuetify(opts);
+Vue.use(Vuetify, opts);
